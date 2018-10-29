e4524de38ac9e658f282bbf74273413765a3e510
Yann Gregoire <yann.gregoire@yahoo.fr>
2018-10-29 15:56:46 +0100

    Dynamic view done

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+%w[papillard ssaunier monsieurpaillard toto].each do |name|
+  proxy "/flats/#{name}.html", '/flats/show.html', locals: { owner: name }, ignore: true
+end
